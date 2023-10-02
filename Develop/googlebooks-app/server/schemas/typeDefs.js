e82750993756a9fb1e9a4d3e85e0970d50e9c2cd
cbailey73 <123669370+cbailey73@users.noreply.github.com>
2023-10-02 16:17:25 -0500

    added apollo connection to app js

---
+++ Develop/googlebooks-app/server/schemas/typeDefs.js
@@ -0,0 +1,42 @@
+const typeDefs = `
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    books: [Book]!
+  }
+
+  type Book {
+    _id: ID
+    authors: String
+    description: String
+    bookId: String
+    image: String
+    link: String
+    title: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    users: [User]
+    user(username: String!): User
+    books(title: String): [Book]
+    bookById(bookId: String!): Book
+    myBooks(userId: ID!): [Book]
+    me: User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook(bookId: String!): Book
+    removeBook(bookId: String!): Book
+  }
+`;
+
+module.exports = typeDefs;
