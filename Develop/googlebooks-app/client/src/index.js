c9522a2f170794905cf780e902ac34357f67e2bb
cbailey73 <123669370+cbailey73@users.noreply.github.com>
2023-10-03 13:58:42 -0500

    updated to modern react

--- Develop/googlebooks-app/client/src/index.js
+++ Develop/googlebooks-app/client/src/index.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import App from './App.jsx';
+// import Profile from './pages/Profile';
+import SavedBooks from './pages/SavedBooks.jsx';
+import SearchBooks from './pages/SearchBooks.jsx';
+import ErrorPage from './pages/ErrorPage.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <SearchBooks />
+      }, {
+        path: '/my-books',
+        element: <SavedBooks />
+      }
+//    {
+      //   path: '/me',
+      //   element: <Profile />
+      // }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+)
